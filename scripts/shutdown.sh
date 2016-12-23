8eefb6d7a7facea74f2e1c4a09bca30c008775d6
Rachael Johnson <rhatchet@users.noreply.github.com>
2016-12-22 21:57:20 -0800

    Add files via upload

---
+++ scripts/shutdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# res=$(rofi -dmenu -p "menu:" -bw 0 -bc "#f3f4f5" -bg "#f3f4f5" -fg "#2f343f" -hlbg "#2f343f" -hlfg "#f3f4f5" -separator-style none -location 0 -width 250 -hide-scrollbar -padding 30 -font "System San Francisco Display 18" < /home/khoaduccao/.config/dmenu)
+# res=$(echo "lock;reload;restart;logout;suspend;shutdown" | rofi -sep ";" -dmenu -p "power:" -bw 0 -bc "#f3f4f5" -bg "#f3f4f5" -fg "#2f343f" -hlbg "#2f343f" -hlfg "#f3f4f5" -separator-style none -location 0 -width 250 -hide-scrollbar -padding 30)
+res=$(echo "lock;reload;restart;logout;suspend;shutdown" | rofi -sep ";" -dmenu -p "power:" -location 0 -width 250 -padding 30)
+
+if [ $res = "lock" ]; then
+    ~/.config/i3/i3-lock/lock
+elif [ $res = "logout" ]; then
+    i3-msg exit
+elif [ $res = "reload" ]; then
+    i3-msg restart
+elif [ $res = "suspend" ]; then
+    systemctl suspend
+elif [ $res = "restart" ]; then
+    systemctl reboot
+elif [ $res = "shutdown" ]; then
+    systemctl poweroff
+fi
+exit 0
